675ec9b84f06a2641c82aacd107e8331bcc25c0a
Aleksandar Bencun <Aleksandar.Bencun@seavus.biz>
2017-11-13 13:59:28 +0100

    Added Category and Question models, controllers and migrations.

---
+++ database/migrations/2017_11_13_124449_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('category_id');
+            $table->integer('difficulty');
+            $table->text('note');
+            $table->boolean('multiselect');
+            $table->json('options');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
