4af78c1118126cbf919eed1ea8ebbd173bdf6bd5
Aleksandar Bencun <Aleksandar.Bencun@seavus.biz>
2017-11-14 16:25:00 +0100

    Grouped the API routes.

--- routes/api.php
+++ routes/api.php
@@ -13,44 +13,52 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
-Route::get('/categories', "CategoryController@index");
-Route::get('/categories/{category}', "CategoryController@show");
-Route::post('/categories', "CategoryController@store");
-Route::put('/categories', "CategoryController@update");
-Route::delete('/categories', "CategoryController@delete");
+Route::post('/login', "AuthController@login");
+Route::post('/logout', "AuthController@logout");
 
-Route::get('/questions/all/{category}', "QuestionController@index");
-Route::get('/questions/{question}', "QuestionController@show");
-Route::post('/questions', "QuestionController@store");
-Route::put('/questions', "QuestionController@update");
-Route::delete('/questions', "QuestionController@delete");
-
-Route::get('/templates', "TestTemplateController@index");
-Route::get('/templates/{template}', "TestTemplateController@show");
-Route::post('/templates', "TestTemplateController@store");
-Route::put('/templates', "TestTemplateController@update");
-Route::delete('/templates', "TestTemplateController@delete");
-
-Route::get('/users', "UsersController@index");
-Route::get('/users/{user}', "UsersController@show");
-Route::post('/users', "UsersController@store");
-Route::put('/users', "UsersController@update");
-Route::delete('/users', "UsersController@delete");
-
-//get all the graded tests for the current user
-Route::get('/test/{user}', "TestController@index");
-//get a specific graded test (for listing of the results)
-Route::get('/test/{user}/{test}', "TestController@read");
-//generate the entire test to work on based on test template id
-Route::get('/test/generate/{user}/{template}', "TestController@generate");
-//this POST updates the complete test (for timed tests and the test finale)
-Route::post('/test', "TestController@store");
-//this updates a single question (for timed questions)
-Route::put('/test/question', "TestController@update");
-//this deletes the test from the DB
-//Route::delete('/test', "TestController@delete");
-//TODO
+Route::middleware(['jwt.auth', 'jwt.refresh'])
+    ->group(function(){
+        Route::get('/categories', "CategoryController@index");
+        Route::get('/categories/{category}', "CategoryController@show");
+        Route::post('/categories', "CategoryController@store");
+        Route::put('/categories', "CategoryController@update");
+        Route::delete('/categories', "CategoryController@delete");
+        
+        Route::get('/questions/all/{category}', "QuestionController@index");
+        Route::get('/questions/{question}', "QuestionController@show");
+        Route::post('/questions', "QuestionController@store");
+        Route::put('/questions', "QuestionController@update");
+        Route::delete('/questions', "QuestionController@delete");
+        
+        Route::get('/templates', "TestTemplateController@index");
+        Route::get('/templates/{template}', "TestTemplateController@show");
+        Route::post('/templates', "TestTemplateController@store");
+        Route::put('/templates', "TestTemplateController@update");
+        Route::delete('/templates', "TestTemplateController@delete");
+        
+        Route::get('/users', "UsersController@index");
+        Route::get('/users/{user}', "UsersController@show");
+        Route::post('/users', "UsersController@store");
+        Route::put('/users', "UsersController@update");
+        Route::delete('/users', "UsersController@delete");
+        
+        //get all the graded tests for the current user
+        Route::get('/test/{user}', "TestController@index");
+        //get a specific graded test (for listing of the results)
+        Route::get('/test/{user}/{test}', "TestController@read");
+        //generate the entire test to work on based on test template id
+        Route::get('/test/generate/{user}/{template}', "TestController@generate");
+        //this POST updates the complete test (for timed tests and the test finale)
+        Route::post('/test', "TestController@store");
+        //this updates a single question (for timed questions)
+        Route::put('/test/question', "TestController@update");
+        //this deletes the test from the DB
+        //Route::delete('/test', "TestController@delete");
+        //TODO
+    });
