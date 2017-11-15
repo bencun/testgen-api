13bd36863b1ab9c9b9f9009722ea208f9cfab32c
Aleksandar Bencun <Aleksandar.Bencun@seavus.biz>
2017-11-15 10:34:25 +0100

    Removed the auth test routes and associated controller functions.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,26 +6,45 @@ use Illuminate\Http\Request;
 use App\User;
 use JWTAuth;
 use \Tymon\JWTAuth\Exceptions\JWTException;
+use App\Exceptions\CustomException;
 
 class AuthController extends Controller
 {
-    public function flogin(){
-        $user = User::find(1);
-        $token = JWTAuth::fromUser($user);
-        return response()->json($token);
+    public function login(){
+        $input = request()->input();
+        
+        try{
+            $name = $input['name'];
+            $password = $input['password'];
+    
+            $user = User::where('name', '=', $name)
+                ->get()
+                ->first();
+            if($user){
+                $pwVerified = password_verify($password, $user->password);
+                if($pwVerified){
+                    $token = JWTAuth::fromUser($user);
+                    return response()->json($token);
+                }
+                else throw new CustomException("Bad name/password combo.");
+
+            }
+            else throw new CustomException("Invalid user.");
+        }
+        catch(CustomException $e){
+            return response()->json($e, 401);
+        }
     }
 
-    public function tlogin(){
+    public function logout(){
+        $input = request()->input();
         try{
             $user = JWTAuth::parseToken()->authenticate();
-            dd($user);
+            JWTAuth::parseToken()->invalidate();
+            return response("OK", 200);
         }
-        catch(JWTException $e){
-            return response()->json("failure", 403);
+        catch(\Exception $e){
+            return response("You are not logged in.", 401);
         }
-    }
-
-    public function login(){
-        
     }
 }
